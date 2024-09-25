26bf493ab12571f5882e51ead0ccdfb7e1da88aa
DovCaio <onlydeadnotalive@gmail.com>
2024-09-25 17:10:17 -0300

    Modularizado o estilo da caixa de livro

--- src/Components/BookBox.tsx
+++ src/Components/BookBox.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import BookBoxStyle from "./BookBox.module.css"
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function BookBox({img, name, description, author, agePublication, type}: any){
@@ -7,14 +7,14 @@ export default function BookBox({img, name, description, author, agePublication,
 
     return (
 
-        <div className="bookbox">
+        <div className={BookBoxStyle.bookbox}>
             <Image src={img} alt="Image de um livro" width="1000" height="1000"/>
             <div>
                 <h2>{name}</h2>
                 <p>{description}</p>
-                <span>{author}</span>
-                <span>{agePublication}</span>
-                <span>{type}</span>
+                <p>Autor: {author}</p>
+                <p>Ano de publicação: {agePublication}</p>
+                <p>Tipo: {type}</p>
             </div>
         </div>
 
