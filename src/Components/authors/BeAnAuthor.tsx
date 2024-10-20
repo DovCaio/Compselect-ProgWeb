728f5c8631d67cccd10ab5b2102d3105c9a7e8e7
DovCaio <onlydeadnotalive@gmail.com>
2024-10-20 17:22:40 -0300

    Feita uma estilização no processo de publicação por parte de um autor

--- src/Components/authors/BeAnAuthor.tsx
+++ src/Components/authors/BeAnAuthor.tsx
@@ -9,6 +9,176 @@ export default function BeAnAuthor()  {
 
                 <h2>Seja um autor</h2>
 
+                <section className="be-an-author-steps">
+                    <h3 id="process-publication-title">Processo de publicação</h3>
+                    <ol className="be-an-author-steps-ol">
+                        <li>
+                            <h3>Proposta do Manuscrito</h3>
+                            <ul>
+                                <li>
+                                O autor, ou grupo de autores, submete uma proposta
+                                 ou um manuscrito completo à editora. Essa proposta 
+                                 geralmente inclui um resumo do livro, sua estrutura,
+                                  público-alvo, e uma amostra de capítulos.
+                                </li>
+                                <li>
+                                A editora avalia a proposta, considerando o alinhamento 
+                                do livro com seu catálogo, o potencial de vendas e a
+                                 relevância do tema para o público. Isso pode envolver
+                                  um processo de revisão por especialistas na área (revisão técnica).
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <h3>Contrato</h3>
+                            <ul>
+                                <li>Se a editora aceitar o projeto, é assinado um contrato que define
+                                     as obrigações e direitos de ambas as partes, como:</li>
+                                <ul>
+                                    <li>
+                                    Direitos autorais (propriedade intelectual),
+                                    </li>
+                                    <li>
+                                    Cronograma de entrega do manuscrito,
+                                    </li>
+                                    <li>
+                                    Royalties (percentual sobre vendas),
+                                    </li>
+                                    <li>
+                                    Direitos de distribuição (física e digital).
+                                    </li>
+                                </ul>
+                            </ul>
+                        </li>
+                        <li>
+                            <h3>
+                                Revisão Técnica
+                            </h3>
+                            <ul>
+                                <li>
+                                    O autor finaliza o manuscrito e o submete à editora.
+
+                                </li>
+                                <li>
+                                O manuscrito passa por uma revisão técnica, onde especialistas
+                                 em computação verificam a precisão, clareza e atualidade do conteúdo.
+                                  Esses revisores podem sugerir ajustes para melhorar a qualidade técnica.
+
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <h3>
+                                Edição de texto
+                            </h3>
+
+                            <ul>
+                                <li>
+                                O manuscrito passa por uma revisão de estilo, gramática e
+                                 coerência geral. O editor pode ajustar a linguagem para que 
+                                 fique mais acessível ao público-alvo, sem comprometer o rigor 
+                                 técnico.
+                                </li>
+                                <li>
+                                O autor revisa e aprova as mudanças sugeridas pela equipe editorial.
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h3>Diagramação e Design</h3>
+                            <ul>
+                                <li>
+                                A equipe de design da editora trabalha na diagramação do livro, incluindo 
+                                o layout das páginas, figuras, gráficos e fórmulas, que são comuns em livros
+                                 de computação.
+                                </li>
+                                <li>
+                                A capa é desenvolvida, muitas vezes em colaboração com o autor, para refletir
+                                 o conteúdo e atrair o público-alvo.
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h3>Revisão final e aprovação</h3>
+                            <ul>
+                                <li>
+                                Após a diagramação, o livro passa por uma última rodada de revisões 
+                                para garantir que não haja erros remanescentes. O autor dá a aprovação
+                                 final antes da publicação.
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h3>Produção</h3>
+                            <ul>
+                                <li>
+                                A versão física do livro é enviada para impressão. Dependendo da demanda,
+                                 pode ser feita uma tiragem inicial pequena ou sob demanda.
+                                </li>
+                                <li>
+                                Paralelamente, a versão digital (eBook) é preparada, o que inclui a formatação 
+                                para diferentes plataformas (PDF, EPUB, Kindle, etc.).
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h3>Marketing e Distribuição</h3>
+                            <ul>
+                                <li>
+                                    A editora promove o livro por meio de canais como redes sociais,
+                                     newsletters, eventos acadêmicos ou de tecnologia, além de
+                                      parcerias com influenciadores da área de computação.
+
+                                </li>
+                                <li>
+                                O livro é distribuído para livrarias físicas e digitais, podendo ser
+                                 vendido diretamente no site da editora ou em plataformas como Amazon.
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h3>Pós-lançamento</h3>
+                            <ul>
+                                <li>
+                                    O autor participa de eventos, palestras, e divulga o livro para aumentar sua visibilidade.
+                                </li>
+                                <li>
+                                    A editora monitora as vendas e paga os royalties ao autor conforme estabelecido no contrato.
+                                </li>
+                            </ul>
+                        </li>
+                    </ol>
+
+                </section>
+                <section className="benefits-and-requirements">
+                    <h3>Benefícios e Requisitos</h3>
+                        <p>
+                        Para publicar um livro em uma editora especializada em computação,
+                        o autor recebe diversos benefícios e deve atender a certos requisitos.
+                        Entre os benefícios, a editora oferece suporte técnico especializado,
+                        garantindo que o conteúdo seja revisado por profissionais da área. 
+                        Além disso, o livro tem distribuição global em plataformas como Amazon,
+                            e a editora promove o autor com campanhas de marketing e eventos, aumentando
+                            a visibilidade da obra. O autor também recebe royalties competitivos e apoio
+                            em design, diagramação, e proteção de direitos
+                            autorais, além da possibilidade de traduzir o livro para outros idiomas
+                                e expandir seu alcance.
+                        </p>
+                        <p>
+                        Quanto aos requisitos, o autor deve apresentar um manuscrito original e
+                        relevante para o campo da computação, demonstrando experiência profissional
+                        ou acadêmica. É necessário submeter uma proposta estruturada com o conteúdo 
+                        detalhado e seguir prazos rigorosos. O autor deve estar disposto a revisar 
+                        o texto com base no feedback da equipe editorial e manter 
+                        exclusividade dos direitos de publicação. Além disso, espera-se que o 
+                        autor participe das atividades de divulgação e esteja disponível para 
+                        atualizações futuras do conteúdo, especialmente em áreas onde o conhecimento
+                        técnico pode rapidamente se desatualizar.
+                        </p>
+                </section>
+                    
         </section>
     )
 }
--- src/Views/Authors.tsx
+++ src/Views/Authors.tsx
@@ -1,10 +1,17 @@
 import AuthorEmphasis from "@/Components/authors/AuthorEmphasis"
 import "./Authors.css"
+import BeAnAuthor from "@/Components/authors/BeAnAuthor"
+import AuthorTestimonyals from "@/Components/authors/AuthorTestimonyals"
 export default function Authors(){
 
 
     return (
-        <AuthorEmphasis/>
+        <>
+            <AuthorEmphasis/>
+            <BeAnAuthor/>
+            <AuthorTestimonyals/>
+
+        </>
     )
 
 } 
