854250a9f7ddd28aec9909a2897a03fa0b690b44
DovCaio <onlydeadnotalive@gmail.com>
2024-11-03 15:08:59 -0300

    Feita um icone de informação

--- src/pages/blog/MakeAComment.tsx
+++ src/pages/blog/MakeAComment.tsx
@@ -1,11 +1,26 @@
 import PostStyle from "./dinamic_post.module.css"
+import { FaInfoCircle } from "react-icons/fa";
+import {Popover, Whisper, Button} from "rsuite"
+import "rsuite/Popover/styles/index.css"
 
 export default function MakeAComment() {
 
+    const infosAboutComment = () => {
+
+        return (
+
+            <Whisper followCursor speaker={<Popover>Depois de enviado o comentário é necessário a confirmação de email, depois seu comentário sera avaliado e só assim postado.</Popover>}>
+                <Button>
+                    <FaInfoCircle/>
+                </Button>
+            </Whisper>
+        )
+
+    }
     return (
         <form className={PostStyle.form}>
             <h3>
-                Deixe seu Cometários
+                Deixe seu Cometários <span>{infosAboutComment()}</span>
             </h3>
             <label htmlFor="name">Nome:</label>
             <input type="text" id="name" />
