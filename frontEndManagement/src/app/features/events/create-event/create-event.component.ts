5e327b4f92d96f6ea17fd10ec67f16a0c30e6f13
DovCaio <onlydeadnotalive@gmail.com>
2024-12-21 02:04:41 -0300

    Feito a insersão de imagens na criaçaõ de eventos

--- frontEndManagement/src/app/features/events/create-event/create-event.component.ts
+++ frontEndManagement/src/app/features/events/create-event/create-event.component.ts
@@ -1,4 +1,4 @@
-import { Component,ChangeDetectionStrategy } from '@angular/core';
+import { Component,ChangeDetectionStrategy} from '@angular/core';
 import {MatDatepickerModule} from '@angular/material/datepicker'; 
 import {MatFormFieldModule} from '@angular/material/form-field'; 
 import {MatInputModule} from '@angular/material/input';
@@ -30,5 +30,45 @@ import {MatDividerModule} from '@angular/material/divider';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CreateEventComponent {
+
+  url: string = '';
+  hideFileDiv: string = "file";
+  hideImageDiv: string = "hidden";
+  fileInputValue: FileList | null = null;
+
+  dragTheFile(event: DragEvent ): void {
+    event.preventDefault();
+    const file = event.dataTransfer?.files[0];
+    if (file && file.type.includes('image') && file.size > 0 && file.size <= 10000000) {
+      this.url = URL.createObjectURL(file);
+      this.hideFileDiv = "hidden";
+      this.hideImageDiv = "div-image";
+
+      const dataTransfer = new DataTransfer();
+      dataTransfer.items.add(file);
+      this.fileInputValue = dataTransfer.files;
+
+    }else {
+      console.log('Arquivo inválido');
+    }
+  }
+
+  changeTheFile(event: Event): void {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (file && file.type.includes('image') && file.size > 0 && file.size <= 10000000) {
+      this.url = URL.createObjectURL(file);
+      this.hideFileDiv = "hidden";
+      this.hideImageDiv = "div-image";
+    }else {
+      console.log('Arquivo inválido');
+    }
+  }
+
+  removeImage():void {
+    this.url = '';
+    this.hideFileDiv = "file";
+    this.hideImageDiv = "hidden";
+    this.fileInputValue = null;
+  }
 
 }
--- frontEndManagement/src/app/features/events/edit-event/edit-event.component.ts
+++ frontEndManagement/src/app/features/events/edit-event/edit-event.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import  Event  from './Event';
+
+const EVENT_DATA: Event[] = [
+  {id: 1, title: 'Hydrogen', date: "1.0079"},
+  {id: 2, title: 'Helium', date: "4.0026" },
+  {id: 3, title: 'Lithium', date: "6.941" },
+  {id: 4, title: 'Beryllium', date: "9.0122"},
+  {id: 5, title: 'Boron', date: "10.811"},
+  {id: 6, title: 'Carbon', date: "12.0107"},
+  {id: 7, title: 'Nitrogen', date: "14.0067"},
+  {id: 8, title: 'Oxygen', date: "15.9994"},
+  {id: 9, title: 'Fluorine', date: "18.9984"},
+  {id: 10,title: 'Neon', date: "20.1797"},
+];
 
 @Component({
   selector: 'app-edit-event',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule
+  ],
   templateUrl: './edit-event.component.html',
   styleUrl: './edit-event.component.css'
 })
 export class EditEventComponent {
-
+  displayedColumns: string[] = ['id', 'title', 'date'];
+  dataSource = EVENT_DATA;
 }
