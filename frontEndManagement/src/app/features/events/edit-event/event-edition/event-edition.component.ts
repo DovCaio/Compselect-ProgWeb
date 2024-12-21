c0d9c7582588de8bf386bc0623c20d79be3e449e
DovCaio <onlydeadnotalive@gmail.com>
2024-12-21 19:13:12 -0300

    Componentizado melhor a edição de eventos

---
+++ frontEndManagement/src/app/features/events/edit-event/event-edition/event-edition.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
+@Component({
+  selector: 'app-event-edition',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
+  ],
+  templateUrl: './event-edition.component.html',
+  styleUrl: './event-edition.component.css'
+})
+export class EventEditionComponent {
+
+}
---
+++ frontEndManagement/src/app/features/events/edit-event/events-table/events-table.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import Event  from '../Event';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+
+import { EditService } from '../../../../shared/event/edit.service';
+
+const EVENT_DATA: Event[] = [
+  {id: 1, title: 'Hydrogen', date: "1.0079"},
+  {id: 2, title: 'Helium', date: "4.0026" },
+  {id: 3, title: 'Lithium', date: "6.941" },
+  {id: 4, title: 'Beryllium', date: "9.0122"},
+  {id: 5, title: 'Boron', date: "10.811"},
+  {id: 6, title: 'Carbon', date: "12.0107"},
+  {id: 7, title: 'Nitrogen', date: "14.0067"},
+  {id: 8, title: 'Oxygen', date: "15.9994"},
+  {id: 9, title: 'Fluorine', date: "18.9984"},
+  {id: 10,title: 'Neon', date: "20.1797"},
+];
+
+@Component({
+  selector: 'events-table',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatTableModule
+  ],
+  templateUrl: './events-table.component.html',
+  styleUrl: './events-table.component.css'
+})
+export class EventsTableComponent {
+  displayedColumns: string[] = ['id', 'title', 'date', 'edit', "delete"];
+  dataSource = EVENT_DATA;
+
+  constructor(private editService: EditService) {}
+
+  eventEdited(id: number, title: string, date: string) {
+    const event: Event = {id: id, title: title, date: date};
+    this.dataSource.splice(id, 1, event);
+    console.log(this.dataSource);
+    this.editService.edit(event);
+  }
+
+  eventDeleted(id: number) {
+    this.dataSource.splice(id, 1);
+    this.editService.delete(id);
+  }
+}
