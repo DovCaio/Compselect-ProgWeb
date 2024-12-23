84d47b2dab5ee3b6eb378c99a4a470e3c1bb5172
DovCaio <onlydeadnotalive@gmail.com>
2024-12-23 16:49:42 -0300

    feat: Feita a página de criação de uma publicação

--- frontEndManagement/src/app/features/publications/add-publication/add-publication.component.ts
+++ frontEndManagement/src/app/features/publications/add-publication/add-publication.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FileInputComponent } from '../../../components/inputs/file-input/file-input.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-add-publication',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FileInputComponent,
+    MatDatepickerModule,
+    RouterLink
+  ],
   templateUrl: './add-publication.component.html',
   styleUrl: './add-publication.component.css'
 })
--- frontEndManagement/src/app/features/publications/publications.component.ts
+++ frontEndManagement/src/app/features/publications/publications.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import  { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-publications',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    RouterLink
+  ],
   templateUrl: './publications.component.html',
   styleUrl: './publications.component.css'
 })
