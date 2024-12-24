84a4c8602a41f97cf3539a238f799bedf084a1ea
DovCaio <onlydeadnotalive@gmail.com>
2024-12-24 18:12:35 -0300

    :broom:cleanup: retirado importe desnecessário em authors add

--- frontEndManagement/src/app/features/authors/add-author/add-author.component.ts
+++ frontEndManagement/src/app/features/authors/add-author/add-author.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 import  {MatButtonModule} from '@angular/material/button';
-import  {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import { FileInputComponent } from '../../../components/inputs/file-input/file-input.component';
 import { AuthorService } from '../../../shared/author/author.service';
 import { AuthorsInputComponent } from '../authors-input/authors-input.component';
 
@@ -11,9 +8,6 @@ import { AuthorsInputComponent } from '../authors-input/authors-input.component'
   standalone: true,
   imports: [
     MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule,
-    FileInputComponent,
     AuthorsInputComponent
   ],
   templateUrl: './add-author.component.html',
