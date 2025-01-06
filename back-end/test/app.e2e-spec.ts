42924786a953753affadad0bdde20581ff415d59
DovCaio <onlydeadnotalive@gmail.com>
2025-01-06 15:08:48 -0300

    :wrench:chore: Feitos as primeiras configurações dos tests.

--- back-end/test/app.e2e-spec.ts
+++ back-end/test/app.e2e-spec.ts
@@ -1,18 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-
+import  * as pactum from "pactum"
 describe('AppController (e2e)', () => {
   let app: INestApplication;
-
-  beforeEach(async () => {
+  let prisma: PrismaService;
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({
+    	whitelist: true
+    }))
     await app.init();
+    await app.listen(3334);
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
+    pactum.request.setBaseUrl("http://localhost:3334")
   });
 
   it('/ (GET)', () => {
